bd945c5d8374db81d2416cfb37511b13b438d731
barnard33-233 <htang0203@gmail.com>
2025-09-04 21:02:27 +0800

    initial commit

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Mo's nix templates";
+  outputs = {self, ...}@args: {
+    templates = {
+      devshell = {
+        path = ./devshell;
+        description = "Empty devshell.";
+      };
+      latex = {
+        path = ./latex;
+        description = "Basic latex template.";
+      };
+    };
+  };
+}
---
+++ latex/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Empty devshell.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }@args :
+  flake-utils.lib.eachDefaultSystem (system: 
+  let
+    pkgs = import nixpkgs { inherit system; };
+  in
+  {
+    devShells.default = pkgs.mkShell {
+      packages = with pkgs; [
+        hello
+      ];
+    };
+  });
+}
